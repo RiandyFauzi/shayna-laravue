beb5cbc3ec332b9c9ebd7fc64fbcfff550bb52d2
riandyfauzi <riand480@gmail.com>
2021-04-04 19:22:10 +0700

    first commit shayna

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <CarouselShayna />
+    <BannerShayna />
+    <InstaShayna />
+     <PartnerShayna />
+      <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import CarouselShayna from "@/components/CarouselShayna.vue";
+import BannerShayna from "@/components/BannerShayna.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import PartnerShayna from "@/components/PartnerShayna.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+    CarouselShayna,
+    BannerShayna,
+    InstaShayna,
+    PartnerShayna,
+      FooterShayna,
+  },
+};
+</script>
---
+++ src/views/ShoppingCart.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <!-- Breadcrumb Section Begin -->
+    <div class="breacrumb-section text-left">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumb-text product-more">
+                         <router-link to="/"><i class="fa fa-home"></i> Home</router-link>
+                        <span>Detail</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Breadcrumb Section Begin -->
+      <ShoppingShayna />
+      <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+import ShoppingShayna from "@/components/ShoppingShayna.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+      FooterShayna,
+        ShoppingShayna,
+  },
+};
+</script>
