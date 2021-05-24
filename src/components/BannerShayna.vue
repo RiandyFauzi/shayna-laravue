6a3dee78275433df226956726e3d608ebeba5338
riandyfauzi <riand480@gmail.com>
2021-05-24 21:55:02 +0700

    first commit

--- src/components/BannerShayna.vue
+++ src/components/BannerShayna.vue
@@ -3,89 +3,58 @@
   <section class="women-banner spad">
     <div class="container-fluid">
       <div class="row">
-        <div class="col-lg-12 mt-5">
-          <carousel class="product-slider" :items='3' :dots="false" :nav="false" :autoplay="true">
-            <div class="product-item">
+        <div class="col-lg-12 mt-5" v-if="products.length > 0">
+          <carousel
+            class="product-slider"
+            :items="3"
+            :dots="false"
+            :nav="false"
+            :autoplay="true"
+          >
+            <div
+              class="product-item"
+              v-for="itemProduct in products"
+              v-bind:key="itemProduct.id"
+            >
               <div class="pi-pic">
-                <img src="img/mickey1.jpg" alt="" />
+                <img v-bind:src="itemProduct.galleries[0].photo" class="img-default" />
                 <ul>
                   <li class="w-icon active">
-                    <a href="#"><i class="icon_bag_alt"></i></a>
+                    <a
+                        @click="
+                          saveKeranjang(
+                            itemProduct.id,
+                            itemProduct.name,
+                            itemProduct.price,
+                            itemProduct.galleries[0].photo
+                          )
+                        "
+                        href="#"
+                        class="primary-btn pd-cart"><i class="icon_bag_alt"></i></a>
                   </li>
                   <li class="quick-view">
-                  <router-link to="/product">+ Quick View</router-link>
+                    <router-link v-bind:to="'/product/'+itemProduct.id">+ Quick View</router-link>
                   </li>
                 </ul>
               </div>
               <div class="pi-text">
-                <div class="catagory-name">Coat</div>
-               <router-link to="/product"> <a href="#">
-                  <h5>Mickey Baggy</h5>
-                </a></router-link>
+                <div class="catagory-name">{{ itemProduct.type }}</div>
+                <router-link to="/product">
+                  <a href="#">
+                    <h5>Mickey Baggy</h5>
+                  </a></router-link
+                >
                 <div class="product-price">
-                  $14.00
+                  {{ itemProduct.price }}
                   <span>$35.00</span>
                 </div>
               </div>
             </div>
-            <div class="product-item">
-              <div class="pi-pic">
-                <img src="img/products/women-2.jpg" alt="" />
-                <ul>
-                  <li class="w-icon active">
-                    <a href="#"><i class="icon_bag_alt"></i></a>
-                  </li>
-                  <li class="quick-view"><a href="#">+ Quick View</a></li>
-                </ul>
-              </div>
-              <div class="pi-text">
-                <div class="catagory-name">Shoes</div>
-                <a href="#">
-                  <h5>Guangzhou sweater</h5>
-                </a>
-                <div class="product-price">$13.00</div>
-              </div>
-            </div>
-            <div class="product-item">
-              <div class="pi-pic">
-                <img src="img/products/women-3.jpg" alt="" />
-                <ul>
-                  <li class="w-icon active">
-                    <a href="#"><i class="icon_bag_alt"></i></a>
-                  </li>
-                  <li class="quick-view"><a href="#">+ Quick View</a></li>
-                </ul>
-              </div>
-              <div class="pi-text">
-                <div class="catagory-name">Towel</div>
-                <a href="#">
-                  <h5>Pure Pineapple</h5>
-                </a>
-                <div class="product-price">$34.00</div>
-              </div>
-            </div>
-            <div class="product-item">
-              <div class="pi-pic">
-                <img src="img/products/women-4.jpg" alt="" />
-                <ul>
-                  <li class="w-icon active">
-                    <a href="#"><i class="icon_bag_alt"></i></a>
-                  </li>
-                  <li class="quick-view"><a href="#">+ Quick View</a></li>
-                  <li class="w-icon">
-                    <a href="#"><i class="fa fa-random"></i></a>
-                  </li>
-                </ul>
-              </div>
-              <div class="pi-text">
-                <div class="catagory-name">Towel</div>
-                <a href="#">
-                  <h5>Converse Shoes</h5>
-                </a>
-                <div class="product-price">$34.00</div>
-              </div>
-          </div>
-            </carousel>
+          </carousel>
+        </div>
+
+        <div class="col-lg-12" v-else>
+          <p>Produk terbaru belum tersedia</p>
         </div>
       </div>
     </div>
@@ -95,11 +64,67 @@
 
 <script>
 import carousel from "vue-owl-carousel";
+import axios from "axios";
 
 export default {
   name: "BannerShayna",
   components: {
     carousel,
   },
+
+  data() {
+    return {
+      products: [],
+      keranjangUser: [],
+    };
+  },
+methods: {
+    changeImage(urlImage) {
+      this.gambar_default = urlImage;
+    },
+
+    setDataPicture(data) {
+      // replace object productDetails dengan data dari API
+      this.productDetails = data;
+      // replace value gambar default dengan data dari API (galleries)
+      this.gambar_default = data.galleries[0].photo;
+    },
+
+    saveKeranjang(idProduct, nameProduct, priceProduct, photoProduct) {
+      var productStored = {
+        id: idProduct,
+        name: nameProduct,
+        price: priceProduct,
+        photo: photoProduct,
+      };
+
+      this.keranjangUser.push(productStored);
+      const parsed = JSON.stringify(this.keranjangUser);
+      localStorage.setItem("keranjangUser", parsed);
+
+      window.location.reload();
+    },
+  },
+  mounted() {
+        if (localStorage.getItem("keranjangUser")) {
+      try {
+        this.keranjangUser = JSON.parse(localStorage.getItem("keranjangUser"));
+      } catch (e) {
+        localStorage.removeItem("keranjangUser");
+      }
+    }
+    axios
+      .get("http://127.0.0.1:8000/api/products")
+      .then((res) => (this.products = res.data.data.data))
+
+      .catch((err) => console.log(err));
+  },
 };
 </script>
+
+<style scoped>
+.img-default{
+  
+  height: 260px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -22,7 +22,17 @@ const routes = [{
             import ( /* webpackChunkName: "about" */ '../views/About.vue')
     },
     {
-        path: '/product',
+        path: '/cart',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/ShoppingCart.vue')
+    },
+    {
+        path: '/product/:id',
+        props: true,
         name: 'Product',
         component: Product
     },
@@ -36,6 +46,7 @@ const routes = [{
         name: 'Success',
         component: Success
     },
+
 ]
 
 const router = new VueRouter({
